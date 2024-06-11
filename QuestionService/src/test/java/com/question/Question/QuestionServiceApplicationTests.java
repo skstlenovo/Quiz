70bd6e40cd14bec585a103e33c9c590201991bf6
skstlenovo <skstlenovo@gmail.com>
2024-06-11 07:33:43 +0530

    first Commit

---
+++ QuestionService/src/test/java/com/question/Question/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.question.Question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
